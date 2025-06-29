50bbc29ef60416fe3520d1c95ff80f049a0ac568
L-a-z-e <yysi8771@gmail.com>
2025-06-29 17:23:16 +0900

    함께 본 상품 구현

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -2,6 +2,7 @@
   <div v-if="loading">로딩 중...</div>
   <div v-if="error">{{ error }}</div>
   <div v-if="product">
+    <!-- 상품 상세 정보 -->
     <h1>{{ product.productName }}</h1>
     <p>가격: {{ product.price }}원</p>
     <p>재고: {{ product.stock }}</p>
@@ -13,34 +14,54 @@
       <button @click="addCart">장바구니 담기</button>
       <p v-if="cartMessage">{{ cartMessage }}</p>
     </div>
+
+    <!-- 추천 상품 -->
+    <div v-if="recommendations.length > 0" class="recommendations">
+      <hr>
+      <h2>함께 본 상품</h2>
+      <ul>
+        <li v-for="recommendation in recommendations" :key="recommendation.id">
+          <router-link :to="`/products/${recommendation.id}`">
+            <strong>{{ recommendation.productName }}</strong> - {{ recommendation.price }}원
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 import type {Product} from "@/types/api.ts";
 import apiClient from "@/api";
 import axios from "axios";
+import {useRoute} from "vue-router";
 
 const props = defineProps<{
   id: string;
 }>();
+const route = useRoute();
 
 const product = ref<Product | null>(null);
+const recommendations = ref<Product[]>([]);
 const quantity = ref<number>(1);
 const loading = ref<boolean>(true);
 const error = ref<string>('');
 const cartMessage = ref<string>('');
 
 onMounted(async () => {
-  await fetchProductDetail();
+  await fetchProductDetail(props.id);
 })
 
-const fetchProductDetail = async () => {
+const fetchProductDetail = async (productId: string) => {
   try{
     loading.value = true;
-    const response = await apiClient.get<Product>(`/products/${props.id}`);
-    product.value = response.data;
+    const [productResponse, recommendResponse] = await Promise.all([
+        apiClient.get<Product>(`/products/${productId}`),
+        apiClient.get<Product[]>(`/products/${productId}/recommendations`)
+    ]);
+    product.value = productResponse.data;
+    recommendations.value = recommendResponse.data;
   } catch (err) {
     console.error(err);
     error.value = '상품 정보를 불러오는 데 실패했습니다.';
@@ -61,7 +82,7 @@ const addCart = async () => {
   } catch (err) {
     console.error(err);
     if (axios.isAxiosError(err)) {
-      if (err.response && err.response.status === 401 || err.response.status === 403) {
+      if (err.response && err.response.status === 401 || err.response?.status === 403) {
         cartMessage.value = '장바구니에 상품을 추가하려면 로그인이 필요합니다.';
       } else {
         cartMessage.value = '장바구니 추가에 실패했습니다.';
@@ -72,6 +93,12 @@ const addCart = async () => {
 
   }
 };
+
+watch(() => props.id, (newId) => {
+  if (route.name === 'ProductDetail') {
+    fetchProductDetail(newId);
+  }
+})
 </script>
 
 <style scoped>
